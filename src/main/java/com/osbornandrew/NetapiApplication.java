851c63dbe8dc7af0cfde19e5ac3aad5a01417e01
akosborn <andrewosborn93@gmail.com>
2019-02-07 22:51:50 -0500

    first commit

---
+++ src/main/java/com/osbornandrew/NetapiApplication.java
@@ -0,0 +1,14 @@
+package com.osbornandrew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetapiApplication.class, args);
+	}
+
+}
+
