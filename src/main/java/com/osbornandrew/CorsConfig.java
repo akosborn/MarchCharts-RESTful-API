38d47155685e7e61e959ecd4ff27ffbe0091316b
akosborn <andrewosborn93@gmail.com>
2019-02-10 05:37:23 -0500

    Removed CORS config for prod

--- src/main/java/com/osbornandrew/CorsConfig.java
+++ src/main/java/com/osbornandrew/CorsConfig.java
@@ -6,10 +6,8 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@Configuration
 public class CorsConfig {
 
-    @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurerAdapter() {
             @Override
