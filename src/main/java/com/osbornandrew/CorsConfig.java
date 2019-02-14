7dbc66e3cc0f428dca3bd1311bfcc838ac0cf6d7
akosborn <andrewosborn93@gmail.com>
2019-02-13 20:20:06 -0500

    Added dev props and added cors config back

--- src/main/java/com/osbornandrew/CorsConfig.java
+++ src/main/java/com/osbornandrew/CorsConfig.java
@@ -6,15 +6,17 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@Configuration
 public class CorsConfig {
 
+    @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedMethods("GET", "OPTIONS")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("*")
                         .allowedHeaders("*");
             }
         };
