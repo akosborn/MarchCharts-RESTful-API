e646f316aa2aeb248e1b2843a28dc3bd4e4e4b6a
akosborn <andrewosborn93@gmail.com>
2019-02-24 12:41:34 -0500

    Added bubble property to Team and MetricsDifference

--- src/main/java/com/osbornandrew/model/Team.java
+++ src/main/java/com/osbornandrew/model/Team.java
@@ -1,6 +1,8 @@
 package com.osbornandrew.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,24 +11,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Data
+@JsonPropertyOrder({"id", "name", "conference", "onBubble", "metrics"})
 @Table(name = "teams")
 public class Team {
 
     @Id
-    @Getter @Setter
     private Long id;
 
-    @Getter @Setter
     private String name;
 
     @JsonIgnoreProperties({"teams"})
     @ManyToOne
     @JoinColumn(name = "conference_id")
-    @Getter @Setter
     private Conference conference;
+
+    @Column(name = "bubble", nullable = false)
+    private boolean isOnBubble;
 
     @JsonIgnoreProperties({"metrics", "conference"})
     @OneToMany(mappedBy = "team")
-    @Getter @Setter
     private List<Metrics> metrics = new ArrayList<>();
 }
